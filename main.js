ac39f94e0aace0021d898937f2802b55d8b29ef4
Aleksandr Tsygankov <tsygankov.aleksandr@gmail.com>
2016-11-18 18:25:23 +0800

    Unfollow all button + redirect to authorization url

---
+++ main.js
@@ -0,0 +1,3 @@
+var unfollow_all = function() {
+  window.location = 'https://api.instagram.com/oauth/authorize/?client_id=0d4bea08d7ec477bb7c91c9e4d5de00a&redirect_uri=http%3A%2F%2Fpistonsky.github.io%2Funfollow-all%2F%23go&response_type=token';
+}
